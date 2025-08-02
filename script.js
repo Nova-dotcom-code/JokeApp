b55864e6e3f36d89ec621b7a06fe343227a1dbf2
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-08-02 13:39:04 +0100

    Modified files: index.html, script.js -fixed the bug in html file in the link to the script.js file -Modified the script.js to call the elements by their IDs and fetch jokes from the API

---
+++ script.js
@@ -0,0 +1,4 @@
+const jokeButton = document.getElementById('joke-button');
+const jokeBox = document.getElementById('jokeBox');
+const apiUrl = 'https://official-joke-api.appspot.com/random_joke';
+
