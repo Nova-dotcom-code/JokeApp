feb3a11df7aff9794daeb86e4d06875645a32f1a
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-08-02 13:44:31 +0100

    modified: script.js Updated the initail text in the jokeBox and added an event listener to the joke button.

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const jokeButton = document.getElementById('joke-button');
 const jokeBox = document.getElementById('jokeBox');
 const apiUrl = 'https://official-joke-api.appspot.com/random_joke';
 
+jokeBox.textContent = 'Click the button to get a joke!😅 🤣 ...';
+jokeButton.addEventListener('click', async ()=> {
+
+})
