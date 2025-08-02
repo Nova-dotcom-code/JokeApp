7076fd4a716162470033ca1cb5ece6a2cf5ca805
Nova-dotcom-code! <mustophaajibola70@gmail.com>
2025-08-02 13:49:35 +0100

    modified: script.js -using try to execute the fetch request and throw new Error if it fails

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const apiUrl = 'https://official-joke-api.appspot.com/random_joke';
 
 jokeBox.textContent = 'Click the button to get a joke!😅 🤣 ...';
 jokeButton.addEventListener('click', async ()=> {
-
+    try {
+        const response = await fetch(apiUrl);
+        if(!response.ok){
+            throw new Error('Network response was not ok')
+        }
+        const jokeData = await response.json();
+        jokeBox.textContent = `😂😂😂${jokeData.setup}-${jokeData.punchline}`;
+    }
 })
